a042be26ef8ba00e1a0c287cf3ae73d162b306d1
salmonrizoev <salmonrizoev7@gmail.com>
2025-07-04 02:02:34 +0000

    class2

---
+++ Class2/fib.py
@@ -0,0 +1,25 @@
+limit=int(input("Enter the number: "))
+
+first=0
+second=1
+print(first)
+print(second)
+next = first + second
+
+while next < limit:
+    print(next)
+    first=second
+    second=next
+    next=first+second
+
+#  0 1 1 2
+
+#     first= 0
+#     second= 1
+#     next = 1
+
+#     first=1
+#     second=1
+#     next=2
+
+    
