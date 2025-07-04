70a06e5af746c04889e1775be686e8bebb5f3278
salmonrizoev <salmonrizoev7@gmail.com>
2025-07-04 02:03:55 +0000

    class2 changes

---
+++ Class2/function.py
@@ -0,0 +1,7 @@
+def hello(a, b, c, d):
+    print("hello", a)
+    print("hello", b)
+    print("hello", c)
+    print("hello", d)
+
+hello("Tim", "Ann", "bard", "Jenny")
---
+++ Class2/test.py
@@ -0,0 +1,8 @@
+def apply_discount(price, discount):
+    discount_sum = price * (discount/100)
+    return discount_sum
+
+price=float(int("enter number: "))
+   x=apply_discount(price, 10)
+elif price >= 200:
+    x=apply_discount(price, 20)
